891272d0748afecfcbd5f8584610abb2a7724c5c
Aayush <aayushpaliwal555@gmail.com>
2025-05-30 11:55:27 +0530

    authentication-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,71 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
+  const { data: session } = authClient.useSession();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      name, 
+      email,
+      password
+    }, {
+      onError: () => {
+        window.alert("Some error");
+      },
+      onSuccess: () => {
+        window.alert("Success")
+      }
+    })
+  }
+
+  const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password
+    }, {
+      onError: () => {
+        window.alert("Some error");
+      },
+      onSuccess: () => {
+        window.alert("Success")
+      }
+    })
+  }
+
+  if(session) {
+    return (
+      <div>
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Sign out</Button>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      Hello
+    <div className="flex flex-col gap-y-10">
+      <div className="py-4 flex flex-col gap-y-4">
+        <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)}  />
+        <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        
+        <Button onClick={onSubmit}>Create User</Button>
+      </div>
+
+      <div className="py-4 flex flex-col gap-y-4">
+        <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        
+        <Button onClick={onLogin}>Login User</Button>
+      </div>
     </div>
   )
 }
