5f914406f1e3bbd0a991320982c109dbedbdd237
Aayush <aayushpaliwal555@gmail.com>
2025-06-05 16:42:35 +0530

    agents-filter

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -8,22 +8,32 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { columns } from "../components/columns";
 import { DataTable } from "../components/data-table";
 import { EmptyState } from "@/components/empty-state";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { DataPagination } from "../components/data-pagination";
 
 
 
 export const AgentsView = () => {
+    const [filters, setFilters] = useAgentsFilters();
 
     const trpc = useTRPC();
     // This is making the request
     // const { data, isLoading, isError } = useQuery(trpc.agents.getMany.queryOptions());
 
     // This has already fetched data, so no need to use isLoading or isError
-    const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+    const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
+        ...filters,
+    }));
 
     return (
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable columns={columns} data={data} />
-            {data.length === 0 && (
+            <DataTable columns={columns} data={data.items} />
+            <DataPagination 
+                page={filters.page}
+                totalPages={data.totalPages}
+                onPageChange={(page) => setFilters({ page })}
+            />
+            {data.items.length === 0 && (
                 <EmptyState 
                     title="Create your first agent"
                     description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call."
